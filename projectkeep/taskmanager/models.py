73866277e268c1b4b8898761faef0990420fb715
Safayet Jamil <work.jamil4@gmail.com>
2021-12-26 01:12:51 +0600

    Initial project

---
+++ projectkeep/taskmanager/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.base import Model
+from django.utils import timezone
+
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    class PostObjects(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status='published')
+
+    options = (
+        ('archive','Archive'),
+        ('published','Published'),
+
+    )
+    category=models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1
+    )
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(null=True)
+    content = models.TextField()
+    slug = models.SlugField(max_length=250,unique_for_date='published')
+    published = models.DateTimeField(default=timezone.now)
+    author= models.ForeignKey(
+        User,on_delete=models.CASCADE,related_name="blog_posts"
+    )
+    status = models.CharField(
+        max_length=10, choices=options, default='published'
+    )
+    objects = models.Manager() # The default manager.
+    postobjects = PostObjects() # The Dahl-specific manager.
+
+    class Meta:
+        ordering=('published',)
+
+    def __str__(self):
+        return self.title
---
+++ projectkeep/taskmanager/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from taskmanager.models import Post
+from .serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
